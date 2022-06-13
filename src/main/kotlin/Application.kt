87dab19ce99c7bfa5ceda00be02f49a6d11303fa
Anton Minko <anton.minko@fiverr.com>
2022-06-13 12:20:10 +0200

    project structure

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,8 @@
+import medium.P0120_Triangle
+
+object Application {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println(P0120_Triangle().minimumTotal(listOf(listOf(3))))
+    }
+}
---
+++ src/main/kotlin/medium/P0120_Triangle.kt
@@ -0,0 +1,19 @@
+package medium
+
+class P0120_Triangle {
+    fun minimumTotal(triangle: List<List<Int>>): Int {
+        val n = triangle.size
+
+        fun minAtPoint(i: Int, j: Int, memo: MutableMap<Pair<Int,Int>,Int>): Int {
+            if (i == n) return 0
+            if (j < 0 || j >= triangle[i].size) return Int.MAX_VALUE
+            val key = Pair(i,j)
+            if (!memo.containsKey(key)) {
+                memo[key] = triangle[i][j] + minOf(minAtPoint(i+1,j,memo), minAtPoint(i+1,j+1,memo))
+            }
+            return memo[key]!!
+        }
+
+        return minAtPoint(0,0, mutableMapOf<Pair<Int,Int>,Int>())
+    }
+}
